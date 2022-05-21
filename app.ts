d5ad3b4f147f872ecbee3a3af2b57fb6340d39ba
mihnevich <dima.mihmih@mail.ru>
2022-05-21 15:25:00 +0300

    Enum

--- app.ts
+++ app.ts
@@ -1,4 +1,26 @@
-const skill: readonly [number, string] = [1, 'dev'];
+enum StatusCode {
+	Success = 200,
+	NotFound = 404,
+	InternalServerError = 500,
+}
 
-const skills: ReadonlyArray<string> = ['Dev', 'QA'];
+const res = {
+	message: 'Платеж успешен!',
+	statusCode: StatusCode.Success,
+};
 
+if (res.statusCode === StatusCode.Success) {
+	console.log(res.message);
+}
+
+
+function compute() {
+	return 3;
+}
+
+const enum Roles {
+	ADMIN = 1,
+	USER = 2
+}
+
+const res2 = Roles.ADMIN;
