35c567185653eaf7b5c1351f65d4b481198c86f8
mihnevich <dima.mihmih@mail.ru>
2022-05-21 14:30:59 +0300

    readonly

--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
-const skill: [number, string] = [1, 'dev'];
+const skill: readonly [number, string] = [1, 'dev'];
 
-const [id, skillName] = skill;
-console.log(id, skillName);
-const arr: [number, string, ...boolean[]] = [2, 'DevOps', true, false, true];
+const skills: ReadonlyArray<string> = ['Dev', 'QA'];
+
