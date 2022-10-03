dd9ced75a6b3f781428be58f08a1b27c92ff9572
Dzimitry Mikhnevich <dima.mihnevich@gmail.com>
2022-10-03 23:07:02 +0300

    union

--- app.ts
+++ app.ts
@@ -1,26 +1,35 @@
-enum StatusCode {
-	Success = 200,
-	NotFound = 404,
-	InternalServerError = 500,
+function logId(id: string | number | boolean) {
+	//сужение union типа
+	if (typeof id === 'string') {
+		console.log(id.toUpperCase);
+	} else if (typeof id === 'number') {
+		console.log(id.toFixed);
+	} else {
+		console.log(id);
+	}
 }
 
-const res = {
-	message: 'Платеж успешен!',
-	statusCode: StatusCode.Success,
-};
-
-if (res.statusCode === StatusCode.Success) {
-	console.log(res.message);
+function errLog(err: string | string[]) {
+	if (Array.isArray(err)) {
+		console.log(err);
+	} else {
+		console.log(err);
+	}
 }
 
-
-function compute() {
-	return 3;
+function errLogObject(obj: { a: number } | { b: number }) {
+	if ('a' in obj) {
+		console.log(obj.a);
+	} else {
+		console.log(obj.b);
+	}
 }
 
-const enum Roles {
-	ADMIN = 1,
-	USER = 2
+function logMultipleIds(a: string | number, b: string | boolean) {
+	if (a === b) {
+		console.log(a);
+	} else {
+		console.log(b);
+	}
 }
 
-const res2 = Roles.ADMIN;
